5d11538bf40ee0062b2bc15b58535350d02bcc58
vthurai <vax.thurai@gmail.com>
2023-02-12 20:30:29 -0500

    added questions to the first lesson plan

--- python/1_basic-data-types/lesson.py
+++ python/1_basic-data-types/lesson.py
@@ -1,17 +1,52 @@
 ### Read the following before starting
 ### Automate The Boring Stuff With Python - Pages 14 to 28
 ### Python All in One For Dummies - Pages 85 to 123
-### Python Crash Course - Pages 3 to 19
+### Python Crash Course - Pages 19 to 36
 
 
 
-## Basic Data Types
+## 1. Basic Data Types
+## 1.a) Number Types
+x = 34 # Integer Type
+y1 = 4 # Integer Type
+y2 = 4.0 # Float Type
+
+# What are the difference between both divisions statements? Why is that?
+# Bonus: Run the below using Python2 and Python 3
+print(x / y1)
+print(x / y2)
+
+# What are the difference between both multiplication statements? Why is that?
+# Bonus: Run the below using Python2 and Python 3
+print(x * y1)
+print(x * y2)
+
+## 1.b) String Type
+# Create a string variable 'name' that will be used in the print statement below
+print(f'Welcome to {name} python code')
+
+# 2. Data type conversion
 
 
-## Data type conversion
+
+## 3. String Manipulation
 
 
-## String Manipulation
+## 4. Date Time and Time zone Offset
+import datetime
 
+## 4.a) Print the current time using the datetime library
 
-## Date Time and Time zone Offset
+## 4.b) Print the time in the above answer in the full date format (e.g. Sunday Febuary 12 2023)
+
+## 4.c) Using the above time, create the following statement using the current time variable
+## 'Right now its HH:MM (AM/PM) on [Weekday], [Month] [Day], [Year]'
+## E.g. 'Right now its 8:23 PM on Sunday, February 12, 2023'
+
+## 4.d) Using the logic above, create the same statement but convert to the approriate time zone in:
+## - Vancouver
+## - Paris
+## - Tokyo
+## E.g. 'Right now in Tokyo its 10:23 AM on Sunday, February 13, 2023'
+
+## 4.e) Write a python statement that will print the current amount of days till Christmas
--- python/README.md
+++ python/README.md
@@ -3,7 +3,9 @@
 ## Textbooks
 
 To request access to the textbooks used for the lessons, request access to the following Google Drive link:
+
 https://drive.google.com/drive/u/0/folders/1fzTTQgeOG1eByZERgydoJ1WjJHo4ouE5
+
 
 ## IDE
 
@@ -13,3 +15,15 @@ For Python, I recommend using Visual Studios Code. VS Code is a fairly light wei
 
 https://code.visualstudio.com/download
 
+### Python Extension
+
+As mentioned earlier, VS Code would need a Python extension to get you started in your python development.
+
+https://code.visualstudio.com/docs/languages/python
+
+
+## Jupyter Notebook
+
+Generally a lot of developers (e.g. Data Scientist, Data Engineers, etc.) use Jupyter notebook to test segments of their python code before wrapping it together in one script. I would recommend giving this extension a try while you practices the lesson files.
+
+https://code.visualstudio.com/docs/datascience/jupyter-notebooks
