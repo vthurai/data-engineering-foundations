643b52e8b2f2331a43611f8f30164fe0b8df180c
Jack <40615502+jconcerto@users.noreply.github.com>
2023-02-14 22:01:44 -0500

    Added challenge questions

---
+++ python/04_functions/challenges.py
@@ -0,0 +1,23 @@
+'''
+QUESTION 1.
+Write a function called is_palindrome.
+is_palindrome takes a string as an argument. It returns True if the given string is a palindrome.
+print(is_palindrome("racecar")) >> True
+print(is_palindrome("aabbcc")) >> False
+'''
+
+def ispalindrome():
+  pass
+
+
+'''
+QUESTION 2.
+Write a function called get_palindromes.
+get_palindromes takes a string as an argument.
+It returns a list of substrings which are palindromes.
+print(get_palindromes("racecar")) >> ['racecar', 'aceca', 'cec']
+print(get_palindromes("aabbbaa")) >> ['aabbbaa', 'aa', 'abbba', 'bbb', 'bb', 'bb', 'aa']
+'''
+
+def get_palindromes():
+  pass
