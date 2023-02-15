11ad18c6c184ff534d12a06016fde8b5a11e31c4
Jack <40615502+jconcerto@users.noreply.github.com>
2023-02-15 00:23:24 -0500

    Added recursion challenges

---
+++ python/05_recursion/challenges.py
@@ -0,0 +1,36 @@
+'''
+QUESTION 1.
+Write a function that returns the nth fibonnacci number (1, 1, 2, 3, 5, 8 ... etc.)
+Use recursion.
+print(fibonnacci(1)) >> 1
+print(fibonnacci(2)) >> 1
+print(fibonnacci(5)) >> 5
+'''
+
+def fibonnacci(n):
+  pass
+
+
+'''
+QUESTION 2.
+We want a function that will sort a list of integers in order from least to greatest.
+We will use a method of sorting called merge sort.
+
+Merge sort will take a list and sort it. To implement merge sort:
+Step 1: pick a point in the middle of the list.
+Step 2: divide the list from the middle into a left sublist and a right sublist.
+Step 3: recursively call merge_sort() on both left and right sublists. Don't forget your base case.
+Step 4: now we need to merge the two lists into one sorted list. To do this:
+  Step 4a: compare the first element of both left and right sublists. You should keep track of their indexes to make things easier.
+  Step 4b: pick the smaller element and place it in the correct position in the original list.
+  Step 4c: move on to the next index. Compare elements again, and continue until both left and right lists are exhausted. 
+  Step 4d: if there are any elements remaining that were not compared, add them to the original list.
+Step 5: The list should now be sorted.
+
+unsorted = [5, 17, 0, -31, 14, 8, 66, -2, 6, 22]
+merge_sort(unsorted)
+print(unsorted) >> [-31, -2, 0, 5, 6, 8, 14, 17, 22, 66]
+'''
+
+def merge_sort(lst):
+  pass
