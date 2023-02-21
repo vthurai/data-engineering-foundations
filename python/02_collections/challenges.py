5d5e4d23dcc7d04f167ba0445ea9e49f57140bc5
vthurai <vax.thurai@gmail.com>
2023-02-20 23:51:02 -0500

    My solution for Section 2 - Challenge - Question 3

--- python/02_collections/challenges.py
+++ python/02_collections/challenges.py
@@ -173,3 +173,135 @@ def get_trip_breakdown(schedule: list(dict())):
                 schedule_dicti[place["location"]]['Night'] += depart_hour % 4
 
     return schedule_dicti
+
+
+def get_trip_breakdown_2(schedule: list(dict())):
+    trip_plan = {}
+    for trip in schedule:
+        location = trip["location"]
+        trip_plan[location] = {}
+        total_hours = (int)((trip["end_time"] - trip["start_time"]).total_seconds() / 3600)
+        offset = trip["start_time"].hour
+        if(offset <= 8):
+            midnight = -offset
+            morning = 0
+            afternoon = 0
+            evening = 0
+            night = 0
+        elif(offset > 8 and offset <= 12):
+            midnight = -8
+            morning = 8 - offset
+            afternoon = 0
+            evening = 0
+            night = 0
+        elif(offset > 12 and offset <= 16):
+            midnight = -8
+            morning = -4
+            afternoon = 12 - offset
+            evening = 0
+            night = 0
+        elif(offset > 16 and offset <= 20):
+            midnight = -8
+            morning = -4
+            afternoon = -4
+            evening = 16 - offset
+            night = 0
+        else:
+            midnight = -8
+            morning = -4
+            afternoon = -4
+            evening = -4
+            night = 20 - offset
+        
+        total_offseted_hours = total_hours + offset
+        while(True):
+            # Midnight
+            if (bool((int)(total_offseted_hours / 8))):
+                midnight += 8
+                total_offseted_hours -= 8
+            else:
+                midnight += total_offseted_hours
+                total_offseted_hours = 0
+                break
+
+            # Morning
+            if (bool((int)(total_offseted_hours / 4))):
+                morning += 4
+                total_offseted_hours -= 4
+            else:
+                morning += total_offseted_hours
+                total_offseted_hours = 0
+                break
+
+            # Afternoon
+            if (bool((int)(total_offseted_hours / 4))):
+                afternoon += 4
+                total_offseted_hours -= 4
+            else:
+                afternoon += total_offseted_hours
+                total_offseted_hours = 0
+                break
+
+            # Evening
+            if (bool((int)(total_offseted_hours / 4))):
+                evening += 4
+                total_offseted_hours -= 4
+            else:
+                evening += total_offseted_hours
+                total_offseted_hours = 0
+                break
+
+            # Night
+            if (bool((int)(total_offseted_hours / 4))):
+                night += 4
+                total_offseted_hours -= 4
+            else:
+                night += total_offseted_hours
+                total_offseted_hours = 0
+                break
+
+        trip_plan[location]["Morning"] = morning
+        trip_plan[location]["Afternoon"] = afternoon
+        trip_plan[location]["Evening"] = evening
+        trip_plan[location]["Night"] = night
+
+    return trip_plan
+
+print(get_trip_breakdown_2([
+    {
+        "location": "Paris",
+        "start_time": datetime.datetime(year=2023, month=9, day=12, hour=13),
+        "end_time": datetime.datetime(year=2023, month=9, day=12, hour=19)
+    },
+    {
+        "location": "Brussels",
+        "start_time": datetime.datetime(year=2023, month=9, day=15, hour=11),
+        "end_time": datetime.datetime(year=2023, month=9, day=19, hour=9)
+    },
+    {
+        "location": "Berlin",
+        "start_time": datetime.datetime(year=2023, month=9, day=19, hour=12),
+        "end_time": datetime.datetime(year=2023, month=9, day=25, hour=10)
+    },
+]))
+
+
+# {
+#    "Paris":{
+#       "Morning":9,
+#       "Afternoon":12,
+#       "Evening":12,
+#       "Night":12
+#    },
+#    "Brussels":{
+#       "Morning":14,
+#       "Afternoon":16,
+#       "Evening":16,
+#       "Night":16
+#    },
+#    "Berlin":{
+#       "Morning":22,
+#       "Afternoon":24,
+#       "Evening":24,
+#       "Night":24
+#    }
